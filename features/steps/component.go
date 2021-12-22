72730ab5b6fc047d251f493b2f353b539a1b7eff
David Subiros <subiros.david@gmail.com>
2021-12-22 11:22:22 +0100

    set component tests config as env vars in the compose yml file instead of the code

--- features/steps/component.go
+++ features/steps/component.go
@@ -30,8 +30,6 @@ const (
 	WaitEventTimeout      = 5 * time.Second  // maximum time that the component test consumer will wait for a kafka event
 )
 
-var MinBrokersHealthy = 1
-
 var (
 	BuildTime string = "1625046891"
 	GitCommit string = "7434fe334d9f51b7239f978094ea29d10ac33b16"
@@ -81,12 +79,6 @@ func (c *Component) initService(ctx context.Context) error {
 		return fmt.Errorf("failed to get config: %w", err)
 	}
 
-	cfg.EncryptionDisabled = true
-	cfg.StopConsumingOnUnhealthy = true
-	cfg.CantabularHealthcheckEnabled = true
-	cfg.KafkaConfig.Addr = []string{"kafka:9092"}
-	cfg.KafkaConfig.ConsumerMinBrokersHealthy = MinBrokersHealthy
-	cfg.KafkaConfig.ProducerMinBrokersHealthy = MinBrokersHealthy
 	cfg.DatasetAPIURL = c.DatasetAPI.ResolveURL("")
 	cfg.CantabularURL = c.CantabularSrv.ResolveURL("")
 	cfg.CantabularExtURL = c.CantabularAPIExt.ResolveURL("")
@@ -114,7 +106,7 @@ func (c *Component) initService(ctx context.Context) error {
 		&kafka.ProducerConfig{
 			BrokerAddrs:       cfg.KafkaConfig.Addr,
 			Topic:             cfg.KafkaConfig.ExportStartTopic,
-			MinBrokersHealthy: &MinBrokersHealthy,
+			MinBrokersHealthy: &cfg.KafkaConfig.ProducerMinBrokersHealthy,
 			KafkaVersion:      &cfg.KafkaConfig.Version,
 			MaxMessageBytes:   &cfg.KafkaConfig.MaxBytes,
 		},
@@ -132,7 +124,7 @@ func (c *Component) initService(ctx context.Context) error {
 			BrokerAddrs:       cfg.KafkaConfig.Addr,
 			Topic:             cfg.KafkaConfig.CsvCreatedTopic,
 			GroupName:         ComponentTestGroup,
-			MinBrokersHealthy: &MinBrokersHealthy,
+			MinBrokersHealthy: &cfg.KafkaConfig.ConsumerMinBrokersHealthy,
 			KafkaVersion:      &cfg.KafkaConfig.Version,
 			Offset:            &kafkaOffset,
 		},

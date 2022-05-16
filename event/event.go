4cb705eed4678b1e7cefcee5c9ff40092b3b8850
jitendar.rawat@methods.co.uk <jitendar.rawat@methods.co.uk>
2022-05-16 13:44:49 +0100

    dimensionIds is now an array of strings

--- event/event.go
+++ event/event.go
@@ -2,20 +2,20 @@ package event
 
 // ExportStart provides an avro structure for a Export Start event
 type ExportStart struct {
-	InstanceID     string `avro:"instance_id"`
-	DatasetID      string `avro:"dataset_id"`
-	Edition        string `avro:"edition"`
-	Version        string `avro:"version"`
-	FilterOutputID string `avro:"filter_output_id"`
-	DimensionIDs   string `avro:"dimension_ids"`
+	InstanceID     string   `avro:"instance_id"`
+	DatasetID      string   `avro:"dataset_id"`
+	Edition        string   `avro:"edition"`
+	Version        string   `avro:"version"`
+	FilterOutputID string   `avro:"filter_output_id"`
+	DimensionIDs   []string `avro:"dimension_ids"`
 }
 
 // CSVCreated provides an avro structure for a CSV Created event
 type CSVCreated struct {
-	InstanceID   string `avro:"instance_id"`
-	DatasetID    string `avro:"dataset_id"`
-	Edition      string `avro:"edition"`
-	Version      string `avro:"version"`
-	RowCount     int32  `avro:"row_count"`
-	DimensionIDs string `avro:"dimension_ids"`
+	InstanceID   string   `avro:"instance_id"`
+	DatasetID    string   `avro:"dataset_id"`
+	Edition      string   `avro:"edition"`
+	Version      string   `avro:"version"`
+	RowCount     int32    `avro:"row_count"`
+	DimensionIDs []string `avro:"dimension_ids"`
 }

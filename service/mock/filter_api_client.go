3e061481fc53ec74d02456eb4f46425e8e4387c3
Joshua Medley <jmedleyx7@gmail.com>
2022-05-18 16:10:04 +0100

    regenerate mocks

--- service/mock/filter_api_client.go
+++ service/mock/filter_api_client.go
@@ -30,6 +30,9 @@ var _ service.FilterAPIClient = &FilterAPIClientMock{}
 // 			GetOutputFunc: func(ctx context.Context, userAuthToken string, serviceAuthToken string, downloadServiceToken string, collectionID string, filterOutputID string) (filter.Model, error) {
 // 				panic("mock out the GetOutput method")
 // 			},
+// 			UpdateFilterOutputFunc: func(ctx context.Context, userAuthToken string, serviceAuthToken string, downloadServiceToken string, filterOutputID string, m *filter.Model) error {
+// 				panic("mock out the UpdateFilterOutput method")
+// 			},
 // 		}
 //
 // 		// use mockedFilterAPIClient in code that requires service.FilterAPIClient
@@ -45,6 +48,9 @@ type FilterAPIClientMock struct {
 
 	// GetOutputFunc mocks the GetOutput method.
 	GetOutputFunc func(ctx context.Context, userAuthToken string, serviceAuthToken string, downloadServiceToken string, collectionID string, filterOutputID string) (filter.Model, error)
+
+	// UpdateFilterOutputFunc mocks the UpdateFilterOutput method.
+	UpdateFilterOutputFunc func(ctx context.Context, userAuthToken string, serviceAuthToken string, downloadServiceToken string, filterOutputID string, m *filter.Model) error
 
 	// calls tracks calls to the methods.
 	calls struct {
@@ -85,10 +91,26 @@ type FilterAPIClientMock struct {
 			// FilterOutputID is the filterOutputID argument value.
 			FilterOutputID string
 		}
+		// UpdateFilterOutput holds details about calls to the UpdateFilterOutput method.
+		UpdateFilterOutput []struct {
+			// Ctx is the ctx argument value.
+			Ctx context.Context
+			// UserAuthToken is the userAuthToken argument value.
+			UserAuthToken string
+			// ServiceAuthToken is the serviceAuthToken argument value.
+			ServiceAuthToken string
+			// DownloadServiceToken is the downloadServiceToken argument value.
+			DownloadServiceToken string
+			// FilterOutputID is the filterOutputID argument value.
+			FilterOutputID string
+			// M is the m argument value.
+			M *filter.Model
+		}
 	}
-	lockChecker       sync.RWMutex
-	lockGetDimensions sync.RWMutex
-	lockGetOutput     sync.RWMutex
+	lockChecker            sync.RWMutex
+	lockGetDimensions      sync.RWMutex
+	lockGetOutput          sync.RWMutex
+	lockUpdateFilterOutput sync.RWMutex
 }
 
 // Checker calls CheckerFunc.
@@ -225,5 +247,56 @@ func (mock *FilterAPIClientMock) GetOutputCalls() []struct {
 	mock.lockGetOutput.RLock()
 	calls = mock.calls.GetOutput
 	mock.lockGetOutput.RUnlock()
+	return calls
+}
+
+// UpdateFilterOutput calls UpdateFilterOutputFunc.
+func (mock *FilterAPIClientMock) UpdateFilterOutput(ctx context.Context, userAuthToken string, serviceAuthToken string, downloadServiceToken string, filterOutputID string, m *filter.Model) error {
+	if mock.UpdateFilterOutputFunc == nil {
+		panic("FilterAPIClientMock.UpdateFilterOutputFunc: method is nil but FilterAPIClient.UpdateFilterOutput was just called")
+	}
+	callInfo := struct {
+		Ctx                  context.Context
+		UserAuthToken        string
+		ServiceAuthToken     string
+		DownloadServiceToken string
+		FilterOutputID       string
+		M                    *filter.Model
+	}{
+		Ctx:                  ctx,
+		UserAuthToken:        userAuthToken,
+		ServiceAuthToken:     serviceAuthToken,
+		DownloadServiceToken: downloadServiceToken,
+		FilterOutputID:       filterOutputID,
+		M:                    m,
+	}
+	mock.lockUpdateFilterOutput.Lock()
+	mock.calls.UpdateFilterOutput = append(mock.calls.UpdateFilterOutput, callInfo)
+	mock.lockUpdateFilterOutput.Unlock()
+	return mock.UpdateFilterOutputFunc(ctx, userAuthToken, serviceAuthToken, downloadServiceToken, filterOutputID, m)
+}
+
+// UpdateFilterOutputCalls gets all the calls that were made to UpdateFilterOutput.
+// Check the length with:
+//     len(mockedFilterAPIClient.UpdateFilterOutputCalls())
+func (mock *FilterAPIClientMock) UpdateFilterOutputCalls() []struct {
+	Ctx                  context.Context
+	UserAuthToken        string
+	ServiceAuthToken     string
+	DownloadServiceToken string
+	FilterOutputID       string
+	M                    *filter.Model
+} {
+	var calls []struct {
+		Ctx                  context.Context
+		UserAuthToken        string
+		ServiceAuthToken     string
+		DownloadServiceToken string
+		FilterOutputID       string
+		M                    *filter.Model
+	}
+	mock.lockUpdateFilterOutput.RLock()
+	calls = mock.calls.UpdateFilterOutput
+	mock.lockUpdateFilterOutput.RUnlock()
 	return calls
 }
